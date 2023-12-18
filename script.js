1b8f5ae4273ad63ec1ea922232cafd66042463c8
EggGreatorex <EggGreatorex@gmail.com>
2023-12-18 10:48:40 +0000

    Fixed bugs to do with changing the colour scheme. Fixed a bug when outputting the projects. Added a feature for switching between light and dark mode.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//--------------------------------------------------------------------------------------
+// CONST DECLARATIONS
 const banner = `--------------------------------------------------------------------------------------------------------------------
 --------------------------------------------------------------------------------------------------------------------
 
@@ -11,21 +13,38 @@ const banner = `----------------------------------------------------------------
 --------------------------------------------------------------------------------------------------------------------
                                            `;
 
-
-
 const helpMessge = ` projects                              links to other projects
 
  themes                               view list of themes
 
  theme set <theme>          set a theme
 
+ mode dark|light                 set the background to light or dark mode
+
  clear                                    clear the terminal
 
- banner                                output the banner`
+ banner                                output the banner
+ `;
+
+ const linksMessage = `
+      password lock                                        https://github.com/EggGreatorex/passwordLock
+      message encrypter                                https://github.com/EggGreatorex/MessageEncypter
+      notes  #work in progress                      https://github.com/EggGreatorex/Notes
+      `;
+
+const themesMessage = `
+
+      midnight
+
+      christmas
+
+      neon
+
+      monochrome
+      `;
 
 
-
-
+//--------------------------------------------------------------------------------------
 
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -43,22 +62,18 @@ async function outputBanner() {
   showWelcomeMessage();
 }
 
+async function showWelcomeMessage() {
+  const welcomeMessage =
+    'Welcome to the terminal website. Type "help" to see a list of avaliable commands.';
 
+  const copyrightMessage = "(c) Ethan Greatorex. Version 0.1";
 
-async function showWelcomeMessage(){
-
-  const welcomeMessage = 'Welcome to the terminal website. Type "help" to see a list of avaliable commands.'
-
-  const copyrightMessage = '(c) Ethan Greatorex. Version 0.1';
-  
   copyrightDiv.textContent = copyrightMessage;
   await sleep(70);
   welcomeDiv.textContent = welcomeMessage;
 }
 
 outputBanner();
-
-
 
 // Terminal divs
 const inputElement = document.getElementById("user-input");
@@ -67,18 +82,17 @@ const welcomeDiv = document.getElementById("welcome");
 const copyrightDiv = document.getElementById("copyright");
 const bannerElement = document.querySelector(".banner");
 
-
 inputElement.addEventListener("keydown", async function (event) {
   if (event.key === "Enter") {
     await handleCommand(inputElement.value);
     inputElement.value = "";
-    }
+  }
 });
 
-async function handleCommand(command){
+async function handleCommand(command) {
   let outputCommand = document.createElement("div");
   let output = document.createElement("div");
-    outputCommand.textContent = `--${command}`;
+  outputCommand.textContent = `--${command}`;
   outputContainer.appendChild(outputCommand);
 
   // Check if the command is "help"
@@ -95,14 +109,15 @@ async function handleCommand(command){
     }
   } else {
     // Process the command and display its output
-    output.textContent = `\n ${await processCommand(command)}`;
-    outputContainer.appendChild(output);
+    output.textContent = `${await processCommand(command)}`;
+    if (output.textContent !== "") {
+      outputContainer.appendChild(output);
+    }
   }
 }
 
-
-
 async function processCommand(command) {
+  const bodyElement = document.body;
   switch (command.toLowerCase()) {
     case "clear":
       for (let i = outputContainer.children.length - 1; i >= 0; i--) {
@@ -123,118 +138,74 @@ async function processCommand(command) {
       return "";
 
     case "projects":
-      const linksMessage = `
-        <a href="https://github.com/EggGreatorex/passwordLock" target="_blank" class="glowText">password lock                                  https://github.com/EggGreatorex/passwordLock</a>
-        <a href="https://github.com/EggGreatorex/MessageEncypter" target="_blank" class="glowText">message encrypter                          https://github.com/EggGreatorex/MessageEncypter</a>
-        <a href="https://github.com/EggGreatorex/Notes" target="_blank" class="glowText">notes #work in progress                 https://github.com/EggGreatorex/Notes</a>
-      `;
 
-      const linksContainer = document.createElement("div");
       const linksMessageLines = linksMessage.split("\n");
 
       for (const line of linksMessageLines) {
         const linkDiv = document.createElement("div");
-        linkDiv.innerHTML = line;
-        linksContainer.appendChild(linkDiv);
+        linkDiv.textContent = line;
+        outputContainer.appendChild(linkDiv);
+        await sleep(70);
       }
-
-      // Append the links container to the output container
-      outputContainer.appendChild(linksContainer);
 
       return "";
 
     case "themes":
-      const themesMessage = `
-      midnight
-
-      christmas
-
-      neon
-
-      monochrome
-      `;
 
       themeMessageLines = themesMessage.split("\n");
       for (const line of themeMessageLines) {
-        outputContainer.textContent += line + "\n";
+        const themeMessageDiv = document.createElement("div");
+        themeMessageDiv.textContent = line;
+        outputContainer.appendChild(themeMessageDiv);
         await sleep(70);
       }
 
       return "";
 
     case "theme set midnight":
-      bannerStyle = document.querySelector(".banner");
-      bannerStyle.style.color = "blue";
-
-      inputStyle = document.getElementById("user-input");
-      inputStyle.style.color = "blue";
-      spanStyle = document.querySelector(".inputSpan");
-      spanStyle.style.color = "blue";
-
-      glowElements = document.getElementsByClassName("glowText");
-      for (var i = 0; i < glowElements.length; i++) {
-        glowElements[i].style.color = "blue";
-        glowElements[i].style.textShadow =
-          "0 0 10px #1f3682, 0 0 20px #1f3682, 0 0 30px #1f3682";
-      }
+      applyThemeStyles("blue", "#1f3682");
       return "Theme set to midnight";
 
     case "theme set christmas":
-      bannerStyle = document.querySelector(".banner");
-      bannerStyle.style.color = "red";
-
-      inputStyle = document.getElementById("user-input");
-      inputStyle.style.color = "green";
-      spanStyle = document.querySelector(".inputSpan");
-      spanStyle.style.color = "red";
-
-      glowElements = document.getElementsByClassName("glowText");
-      for (var i = 0; i < glowElements.length; i++) {
-        glowElements[i].style.color = "red";
-        glowElements[i].style.textShadow =
-          "0 0 10px #23b10a, 0 0 20px #23b10a, 0 0 30px #23b10a";
-      }
+      applyThemeStyles("red", "#23b10a");
       return "Theme set to Christmas";
 
     case "theme set neon":
-      // Add Neon theme styles
-      bannerStyle = document.querySelector(".banner");
-      bannerStyle.style.color = "cyan";
-
-      inputStyle = document.getElementById("user-input");
-      inputStyle.style.color = "purple";
-      spanStyle = document.querySelector(".inputSpan");
-      spanStyle.style.color = "cyan";
-
-      glowElements = document.getElementsByClassName("glowText");
-      for (var i = 0; i < glowElements.length; i++) {
-        glowElements[i].style.color = "cyan";
-        glowElements[i].style.textShadow =
-          "0 0 10px #8c00ff, 0 0 20px #8c00ff, 0 0 30px #8c00ff";
-      }
+      applyThemeStyles("cyan", "#8c00ff");
       return "Theme set to neon";
 
     case "theme set monochrome":
-      // Add Monochrome theme styles
-
-      // Add Neon theme styles
-      bannerStyle = document.querySelector(".banner");
-      bannerStyle.style.color = "white";
-
-      inputStyle = document.getElementById("user-input");
-      inputStyle.style.color = "white";
-      spanStyle = document.querySelector(".inputSpan");
-      spanStyle.style.color = "white";
-
-      glowElements = document.getElementsByClassName("glowText");
-      for (var i = 0; i < glowElements.length; i++) {
-        glowElements[i].style.color = "white";
-        glowElements[i].style.textShadow =
-          "0 0 10px #000000, 0 0 20px #000000, 0 0 30px #000000";
-      }
+      applyThemeStyles("white", "#000000");
       return "Theme set to monochrome";
 
     default:
       return 'Command not found. Type "help" for assistance.';
+
+    case "mode light":
+      bodyElement.style.backgroundColor = "rgb(232, 232, 232)";
+      return "Light mode activated --> only strange people use light mode :>";
+
+    case "mode dark":
+      bodyElement.style.backgroundColor = "rgb(28, 28, 28)";
+      return "Dark mode activated";
+  }
+}
+
+function applyThemeStyles(textColor, shadowColor) {
+  // Add Neon theme styles
+  bannerStyle = document.querySelector(".banner");
+  bannerStyle.style.color = textColor;
+
+  inputStyle = document.getElementById("user-input");
+  inputStyle.style.color = textColor;
+  spanStyle = document.querySelector(".inputSpan");
+  spanStyle.style.color = textColor;
+
+  glowElements = document.getElementsByClassName("glowText");
+  for (var i = 0; i < glowElements.length; i++) {
+    glowElements[i].style.color = textColor;
+    glowElements[
+      i
+    ].style.textShadow = `0 0 10px ${shadowColor}, 0 0 20px ${shadowColor}, 0 0 30px ${shadowColor}`;
   }
 }
